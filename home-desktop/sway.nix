3010d0b78a37fd0248bef918666ae65b006f6f65
John Evans <github@thejevans.slmail.me>
2023-11-14 12:35:22 -0700

    added initial sway config

--- home-desktop/sway.nix
+++ home-desktop/sway.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   # Import home-desktop/common, which imports home-common.
@@ -9,22 +9,22 @@
     config = rec {
       modifier = "Mod4";
 
-      keybindings = let
-        modifier = config.wayland.windowManager.sway.config.modifier;
-      in lib.mkOptionDefault {
-        "${modifier}+Return" = "exec ${pkgs.foot}/bin/foot";
-        "${modifier}+Shift+q" = "kill";
-        "${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
-
-        # Brightness
-        "XF86MonBrightnessDown" = "exec light -U 10";
-        "XF86MonBrightnessUp" = "exec light -A 10";
-
-        # Volume
-        "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
-        "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'";
-        "XF86AudioMute" = "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
-      };
+#      keybindings = let
+#        modifier = config.wayland.windowManager.sway.config.modifier;
+#      in lib.mkOptionDefault {
+#        "${modifier}+Return" = "exec ${pkgs.foot}/bin/foot";
+#        "${modifier}+Shift+q" = "kill";
+#        "${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
+#
+#        # Brightness
+#        "XF86MonBrightnessDown" = "exec light -U 10";
+#        "XF86MonBrightnessUp" = "exec light -A 10";
+#
+#        # Volume
+#        "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
+#        "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'";
+#        "XF86AudioMute" = "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
+#      };
 
       # Use foot as default terminal
       terminal = "foot"; 
@@ -34,7 +34,6 @@
       ];
     };
   };
-
 
   home.packages = with pkgs; [
     element-desktop
