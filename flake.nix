5ed7d2702a91ff7e04705f1d76adc9f8bc505ca0
John Evans <github@thejevans.slmail.me>
2024-03-05 03:14:47 -0700

    added cosmic pre-alpha. currently broken. stick with plasma

--- flake.nix
+++ flake.nix
@@ -3,14 +3,18 @@
   inputs = {
     nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager?ref=master";
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixos-hardware, home-manager, nur, ... }:
+  outputs = inputs@{ self, nixpkgs, nixos-cosmic, nixos-hardware, home-manager, nur, ... }:
   {
     nixosConfigurations = {
       mujina = nixpkgs.lib.nixosSystem {
@@ -20,6 +24,16 @@
           ./host-mujina
           nixos-hardware.nixosModules.framework-12th-gen-intel
 
+          {
+            nix.settings = {
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [
+                  "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+                ];
+              };
+          }
+
+          nixos-cosmic.nixosModules.default
 
           { nixpkgs.overlays = [ nur.overlay ]; }
 
--- userspace-desktop/cosmic.nix
+++ userspace-desktop/cosmic.nix
@@ -7,14 +7,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Open ports for KDEConnect
-  networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-  networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
@@ -23,4 +15,7 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+
+  services.xserver.displayManager.cosmic-greeter.enable = true;
+  services.xserver.desktopManager.cosmic.enable = true;
 }
