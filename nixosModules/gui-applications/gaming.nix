303071e4a41c6534449f993068cdf241573cf817
John Evans <thejevans@pm.me>
2024-06-01 17:26:03 -0600

    added sunshine to gaming config

--- nixosModules/gui-applications/gaming.nix
+++ nixosModules/gui-applications/gaming.nix
@@ -30,7 +30,42 @@
       lutris
       heroic
       bottles
+
+      # game streaming
+      sunshine
+      moonlight-qt
     ];
+
+    security.wrappers.sunshine = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_sys_admin+p";
+      source = "${pkgs.sunshine}/bin/sunshine";
+    };
+
+    services.avahi.publish.enable = true;
+    services.avahi.publish.userServices = true;
+
+    # sunshine networking settings
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [ 47984 47989 47990 48010 ];
+      allowedUDPPortRanges = [
+        { from = 47998; to = 48000; }
+        #{ from = 8000; to = 8010; }
+      ];
+    };
+
+    systemd.user.services.sunshine = {
+      description = "Sunshine self-hosted game stream host for Moonlight";
+      startLimitBurst = 5;
+      startLimitIntervalSec = 500;
+      serviceConfig = {
+        ExecStart = "${config.security.wrapperDir}/sunshine";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+    };
 
     programs.gamemode.enable = true;
 
