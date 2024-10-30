6ff59b3453fa91bafff6ebc171254dd6024e2a40
John Evans <thejevans@pm.me>
2024-10-29 23:13:16 -0600

    removed hibernate udev. hibernate + zfs = bad time

--- hosts/generic/personal_laptop.nix
+++ hosts/generic/personal_laptop.nix
@@ -11,9 +11,5 @@
 
   options = {};
 
-  config = {
-    services.udev.extraRules = ''
-      SUBSYSTEM=="power_supply" ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl hibernate"
-    '';
-  };
+  config = {};
 }
